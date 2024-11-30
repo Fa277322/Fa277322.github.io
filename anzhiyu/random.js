53bbe320a2fde889a55f2dc77a888123f67f497d
Fa277322 <2773224074@qq.com>
2024-11-30 16:25:37 +0800

    Site updated: 2024-11-30 16:25:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/30/Linux基础笔记/","2024/11/30/hello-world/"];function toRandomPost(){
+var posts=["undefined/Linux基础笔记/","undefined/这是另一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
