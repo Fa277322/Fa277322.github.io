d63c1f9b7703d7b1928de67b81447f210b0b40b9
Fa277322 <2773224074@qq.com>
2024-11-30 18:09:45 +0800

    Site updated: 2024-11-30 18:09:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/Linux基础笔记/","undefined/这是另一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
+var posts=["undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
