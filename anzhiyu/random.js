49b5fbde09f37af7a62d70812f501a3e5ac918f4
Fa277322 <2773224074@qq.com>
2024-12-01 16:30:09 +0800

    Site updated: 2024-12-01 16:30:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/","undefined/内网穿透笔记/"];function toRandomPost(){
+var posts=["undefined/内网穿透笔记/","undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
