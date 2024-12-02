6c489f8b1f7bd8dd299d4ebb7962aa6ab9bb7344
Fa277322 <2773224074@qq.com>
2024-12-02 16:16:30 +0800

    Site updated: 2024-12-02 16:16:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/机场推荐/","undefined/内网穿透笔记/","undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/","undefined/笔记/","undefined/科学上网/"];function toRandomPost(){
+var posts=["undefined/科学上网/","undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/","undefined/内网穿透笔记/","undefined/机场推荐/","undefined/笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
