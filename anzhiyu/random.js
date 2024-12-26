a3bf60d7186a33ac0217796f9e1d1c24e0e406bf
Fa277322 <2773224074@qq.com>
2024-12-26 16:43:04 +0800

    Site updated: 2024-12-26 16:43:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/","undefined/Windows Server 2022 FTP服务器的搭建与使用/","undefined/远程进行域管理/","undefined/配置DNS服务器进行域名解析+配置邮件服务器/","undefined/内网穿透笔记/","undefined/科学上网/","undefined/机场推荐/","undefined/笔记/"];function toRandomPost(){
+var posts=["undefined/Windows Server 2022配置Web服务器(IIS+php5.6+MySQL)笔记/","undefined/Windows Server 2022 FTP服务器的搭建与使用/","undefined/远程进行域管理/","undefined/配置DNS服务器进行域名解析+配置邮件服务器/","undefined/内网穿透笔记/","undefined/虚拟化技术与应用实训/","undefined/科学上网/","undefined/机场推荐/","undefined/笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
